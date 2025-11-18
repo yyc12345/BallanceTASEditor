86ea296a1b12e034fb047e0c929c53c1a1ecc823
yyc12345 <yyc12321@outlook.com>
2025-11-18 20:53:42 +0800

    refactor: migrate project to .net core 8 to resolve the issue that we can not use CommunityToolkit.Mvvm

---
+++ BallanceTasEditor/Converters/NullableIntStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BallanceTasEditor.Converters {
+    public class NullableIntStringConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BallanceTasEditor/ViewModels/NewFileDialog.cs
+++ BallanceTasEditor/ViewModels/NewFileDialog.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 
 namespace BallanceTasEditor.ViewModels {
     public partial class NewFileDialog : ObservableObject {
-        [ObservableProperty]
-        private int count;
-
-        [ObservableProperty]
-        private int fps;
-
         public NewFileDialog() {
             Count = 10000;
             // 132 or 264
             Fps = 264;
         }
+
+        [ObservableProperty]
+        private int count;
+
+        [ObservableProperty]
+        private int fps;
 
     }
 }
